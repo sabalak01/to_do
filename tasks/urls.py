b6372f8c2241feb8989e856e817281e34afcae2d
sabalak01 <sssabalak@mail.ru>
2025-04-02 16:22:59 +0500

    second

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('new/', views.create_task,name='create_task'),
+    path('task/<int:task_id>/complete', views.complete_task, name='complete_task'),
+    path('task/<int:task_id>', views.task_description, name='task_description'),
+    path('task/<int:task_id>/delete', views.delete_task,name='delete_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .models import Task
 from django.contrib.auth.decorators import login_required
 from .forms import TaskForm
+from django.shortcuts import get_object_or_404
 
 
 def task_list(request):
@@ -25,3 +26,18 @@ def create_task(request):
         form = TaskForm()
     return render(request,'tasks/create_task.html', {'form': form})
 
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    task.status = 'completed'
+    task.save()
+    return redirect('task_list')
+
+def task_description(request,task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    return render(request, 'tasks/task_description.html',{'task': task})
+
+def delete_task(request,task_id):
+    task = get_object_or_404(Task,id=task_id, user=request.user)
+    task.delete()
+    return redirect('task_list')
